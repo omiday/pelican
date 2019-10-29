917136d09deb469eb61994628aa2f23d57b66e15
Viorel Tabara <viorel.tabara@infrabit.net>
2019-10-29 16:40:18 -0700

    Merge changes from remote.

--- pelican/readers.py
+++ pelican/readers.py
@@ -265,13 +265,10 @@ class RstReader(BaseReader):
         extra_params = {'initial_header_level': '2',
                         'syntax_highlight': 'short',
                         'input_encoding': 'utf-8',
-<<<<<<< HEAD
-=======
                         'language_code': self._language_code,
                         'halt_level': 2,
                         'traceback': True,
                         'warning_stream': StringIO(),
->>>>>>> 01eb08c42b543450ee5e0e3de3854526708a6711
                         'embed_stylesheet': False}
         user_params = self.settings.get('DOCUTILS_SETTINGS')
         if user_params:
@@ -284,7 +281,6 @@ class RstReader(BaseReader):
         pub.set_components('standalone', 'restructuredtext', 'html')
         pub.process_programmatic_settings(None, extra_params, None)
         pub.set_source(source_path=source_path)
-<<<<<<< HEAD
         status = user_params.get('enable_exit_status', False)
         try:
             pub.publish(enable_exit_status=status)
@@ -304,9 +300,6 @@ class RstReader(BaseReader):
                 '\nUse "--debug" to display a stack trace.',
                 exc_info=self.settings.get('DEBUG', False))
             raise
-=======
-        pub.publish()
->>>>>>> 01eb08c42b543450ee5e0e3de3854526708a6711
         return pub
 
     def read(self, source_path):
